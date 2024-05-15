212669c0bb01b4d8e36432ee242885a0620f3fe6
StefanVarga <163822400+StefanVarga@users.noreply.github.com>
2024-05-15 19:47:09 +0200

    Wall.java

---
+++ projekt/src/main/java/org/example/Wall.java
@@ -0,0 +1,12 @@
+package org.example.logic;
+
+import java.awt.*;
+
+public class Wall extends Entity {
+    public Wall(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
