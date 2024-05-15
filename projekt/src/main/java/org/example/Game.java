f9b11da82db23e142eb0303dd897039228c3d2b9
StefanVarga <163822400+StefanVarga@users.noreply.github.com>
2024-05-15 19:42:18 +0200

    Update Game.java

--- projekt/src/main/java/org/example/Game.java
+++ projekt/src/main/java/org/example/Game.java
@@ -1,73 +1,35 @@
 package org.example;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.event.*;
 
+public class Game {
+    GameLogic logic;
+    GameGraphics graphics;
 
-public class Game implements Runnable{
     public static void main(String[] args) {
         new Game();
     }
-    private GameGraphics graphics;
-    private GameLogic logic;
-    Thread gameThread;
-    int SET_FPS = 60;
+
+
 
     public Game() {
-        this.logic = new GameLogic();
-        this.graphics = new GameGraphics(logic);
-        gameThread = new Thread(this);
-        gameThread.start();
-        run();
-    }
+        logic = new GameLogic();
+        graphics = new GameGraphics(logic);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    graphics.render(logic);
+                    logic.update();
 
-    public void run() {
-        /*double drawInterval = 1000000000/SET_FPS;
-        double delta = 0;
-        long lastTime = System.nanoTime();
-        long currentTime;
-        long timer = 0;
-        int drawCount = 0;
-
-        while (gameThread != null) {
-            currentTime = System.nanoTime();
-            delta += (currentTime - lastTime) / drawInterval;
-            timer += (currentTime - lastTime);
-            lastTime = currentTime;
-
-            if(delta >= 1) {
-                update();
-                repaint();
-                delta--;
-                drawCount++;
+                    try {
+                        Thread.sleep(1000 / 60);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
             }
-            if (timer >= 1000000000) {
-                System.out.println("FPS:" + drawCount);
-                drawCount = 0;
-                timer = 0;
-            }
-        }*/
-        while(true) {
-            long lastFrame = System.nanoTime();
-            double timePerFrame = 1000000000 / SET_FPS;
-
-            if (System.nanoTime() - lastFrame == timePerFrame) {
-                System.out.println("FPS: "+ timePerFrame);
-                update();
-            }
-        }
-    }
-
-    private void update() {
-        graphics.render(logic);
-        logic.update();
-    }
-
-    public GameGraphics getGraphics() {
-        return graphics;
-    }
-
-    public GameLogic getLogic() {
-        return logic;
+        });
+        thread.start();
     }
 }
