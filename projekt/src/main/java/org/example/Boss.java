277df29c4b29af269cf940ee178066f385adf094
StefanVarga <163822400+StefanVarga@users.noreply.github.com>
2024-05-15 19:41:02 +0200

    Update Boss.java

--- projekt/src/main/java/org/example/Boss.java
+++ projekt/src/main/java/org/example/Boss.java
@@ -1,4 +1,11 @@
-package org.example;
+package org.example.logic;
 
-public class Boss {
+public class Boss extends Entity {
+    public Boss(int x, int y, int width, int height, String file) {
+        this.x = x;
+        this.y = y;
+        this.width = 64;
+        this.height = 64;
+        setImage(file);
+    }
 }
