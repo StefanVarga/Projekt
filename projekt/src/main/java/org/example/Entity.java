022514a6c4e79b1cdbb4fad0a250f601f5028d01
StefanVarga <163822400+StefanVarga@users.noreply.github.com>
2024-05-15 19:41:40 +0200

    Update Entity.java

--- projekt/src/main/java/org/example/Entity.java
+++ projekt/src/main/java/org/example/Entity.java
@@ -1,4 +1,27 @@
-package org.example;
+package org.example.logic;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Entity {
+    public int x, y, width, height;
+    BufferedImage img;
+
+    public void draw(Graphics g) {
+        g.drawImage(img, x, y, width, height, null);
+    }
+    public void setImage(String file) {
+        try {
+            img = ImageIO.read(new File("src/main/resources/" + file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public Rectangle getRectangle() {
+        return new Rectangle(x, y, width, height);
+    }
 }
