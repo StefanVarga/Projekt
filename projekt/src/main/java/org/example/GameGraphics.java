f30cb9450e8aaef673639e46baca6ca545db12d0
StefanVarga <163822400+StefanVarga@users.noreply.github.com>
2024-05-15 19:43:02 +0200

    Update GameGraphics.java

--- projekt/src/main/java/org/example/GameGraphics.java
+++ projekt/src/main/java/org/example/GameGraphics.java
@@ -1,20 +1,43 @@
 package org.example;
 
+import org.example.logic.Boss;
+import org.example.logic.Shot;
+import org.example.logic.Wall;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 
 public class GameGraphics extends JFrame {
-    private GameLogic logic;
+    GameLogic logic;
+    BufferedImage background;
+    Draw draw;
 
-    public GameGraphics(GameLogic logic) throws HeadlessException {
+    GameGraphics(GameLogic logic) throws HeadlessException {
+
         this.logic = logic;
-        new Draw();
+        this.draw = new Draw();
 
-        setSize(1400, 800);
+        setSize(1094, 536);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
-        setTitle("Game");
+        setFocusable(true);
+        requestFocus();
+        setTitle("Hra");
+
+        add(draw);
+        addKeyListener(logic.getPlayer());
+
+        try {
+            background = ImageIO.read(new File("src/main/resources/background1.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void render(GameLogic logic) {
@@ -22,10 +45,16 @@ public class GameGraphics extends JFrame {
         repaint();
     }
 
-    private class Draw extends JPanel{
+    public class Draw extends JPanel {
+
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
+            g.drawImage(background, 0, 0, 1080, 500, null);
+            logic.player.draw(g);
+            for (Boss boss : logic.bosses) {
+                boss.draw(g);
+            }
         }
     }
 }
